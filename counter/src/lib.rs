08cc910a42a092e549db514cb581d2ac6d58cc32
Maximiliano Capodacqua <kapomaxi1@gmail.com>
2023-08-01 22:01:04 -0500

    Implementing redis to sqlite

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -1,12 +1,25 @@
 use anyhow::Result;
 use bytes::Bytes;
-use spin_sdk::redis_component;
+use spin_sdk::{
+    redis_component,
+    sqlite::{Connection, ValueParam},
+};
 use std::str::from_utf8;
 
-/// A simple Spin Redis component.
+
 #[redis_component]
 fn on_message(message: Bytes) -> Result<()> {
-    println!("{}", from_utf8(&message)?);
-    // Implement me ...
+    let message_str = from_utf8(&message)?;
+    println!("Message received: {}", message_str);
+
+    let execute_params = [
+        ValueParam::Text(message_str),
+    ];
+    let connection = Connection::open_default()?;
+    connection.execute(
+        "INSERT INTO votes(name, votes) VALUES(?,1) ON CONFLICT(name) DO UPDATE SET votes=votes+1;",
+        execute_params.as_slice(),
+    )?;
+
     Ok(())
 }
