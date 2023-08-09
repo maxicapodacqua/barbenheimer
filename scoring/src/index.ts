1d52561ac86155b558e664346db7ee475c846bb9
Maximiliano Capodacqua <kapomaxi1@gmail.com>
2023-08-09 11:16:09 -0500

    Fixing response of scoring endpoint

--- scoring/src/index.ts
+++ scoring/src/index.ts
@@ -2,13 +2,30 @@ import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk";
 import { Sqlite } from "@fermyon/spin-sdk";
 
 
-const encoder = new TextEncoder()
+const encoder = new TextEncoder();
+
+type Response = {
+    name: string,
+    votes: number,
+}[];
 
 export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
 
     const conn = Sqlite.openDefault();
-    const result = conn.execute("SELECT * FROM votes", []);
-    const json = JSON.stringify(result.rows);
+    let result = conn.execute("SELECT name, votes FROM votes WHERE name IN (? , ?)", ["barbie", "oppenheimer"]);
+
+    let jsonObj :Response= [];
+    if (result.rows.length) {
+        const rows = result.rows as unknown as Array<any>;
+        jsonObj = rows.map((row) => {
+            return {
+                name: row[0] || '',
+                votes: row[1] || 0,
+            }
+        });
+    }
+
+    const json = JSON.stringify(jsonObj);
 
     return {
         status: 200,
