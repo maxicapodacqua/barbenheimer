ad98251abf2d4c1d852358b73db951e1dccd4a5d
Maximiliano Capodacqua <kapomaxi1@gmail.com>
2023-08-13 21:30:00 -0500

    More progress on UI

---
+++ site/src/lib/Card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let imgPath: string;
+    export let title: string;
+</script>
+
+ <div class="card w-full lg:card-side bg-base-100 shadow-xl">
+    <figure><img class="w-98 md:h-60" src={imgPath} alt={title} /></figure>
+    <div class="card-body">
+      <h2 class="card-title">{title}</h2>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Vote!</button>
+      </div>
+    </div>
+  </div>
--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { PageData } from "./$types";
     import { invalidate } from "$app/navigation";
+    import Card from "$lib/Card.svelte";
 
     export let data: PageData;
 
@@ -24,45 +25,14 @@
 </script>
 
 <div class="container mx-auto my-4">
-    <div class="flex w-full">
-        <div
-            class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
-        >
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img class="h-44" src="oppenheimer.jpeg" alt="Oppenheimer" />
-                </figure>
-                <div class="card-body">
-                    <h2 class="card-title">Oppenheimer</h2>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Vote!</button>
-                    </div>
-                </div>
-            </div>
+    <div class="columns-2xl gap-5 gap-y-5">
 
-        </div>
-        <div class="divider divider-horizontal">OR</div>
-        <div
-            class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
-        >
+        <Card title='Oppenheimer' imgPath="oppenheimer.jpeg"/>
 
-        <div class="card w-96 bg-base-100 shadow-xl">
-            <figure>
-                <img class="h-44" src="barbie.webp" alt="Barbie" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">Barbie</h2>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Vote!</button>
-                </div>
-            </div>
-        </div>
-        
-
-        </div>
+        <Card title='Barbie' imgPath="barbie.webp"/>
     </div>
 </div>
-<div>
+<!-- <div>
     votes: {JSON.stringify(data)}
 </div>
 <button on:click={stopInterval} class="btn">stop refresh</button>
@@ -73,4 +43,4 @@
     <button on:click={() => vote("oppenheimer")} class="btn btn-primary"
         >Oppenheimer</button
     >
-</div>
+</div> -->
